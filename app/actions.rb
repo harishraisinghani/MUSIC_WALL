790eed00cf3d494acd79b64a84f2eb38a32fd0ea
harishraisinghani <harishraisinghani@gmail.com>
2016-08-08 18:13:59 +0000

    added wall path to actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,8 @@
 # Homepage (Root path)
-get '/' do
-  erb :index
+get '/' do #Note the get '/' is our HTTP request
+  erb(:index) 
+end
+
+get '/wall' do
+  'This will be the public music wall'
 end
