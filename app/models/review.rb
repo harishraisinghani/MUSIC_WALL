3f21f9a67ca4d677968a9e9b19134965f2c969e3
harishraisinghani <harishraisinghani@gmail.com>
2016-08-10 02:53:44 +0000

    updated ratings display on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,5 +6,6 @@ class Review < ActiveRecord::Base
   validates :user, presence: true
   validates :track, presence: true, uniqueness: { scope: :user, message: "only one review per user" }
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :content, length: { in: 4..500 } 
 
 end
